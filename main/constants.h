ad16c6b9501c8bdbba5cc700e9e3730d6faeb2ee
Igor Timofeev <methodman1@mail.ru>
2025-10-16 15:34:40 +0300

    заебумба

--- main/constants.h
+++ main/constants.h
@@ -8,25 +8,27 @@
 namespace pizda {
 	class constants {
 		public:
-			class i2c {
+			class spi {
 				public:
-					constexpr static gpio_num_t sda = GPIO_NUM_8;
-					constexpr static gpio_num_t scl = GPIO_NUM_9;
+					constexpr static gpio_num_t mosi = GPIO_NUM_10;
+					constexpr static gpio_num_t clock = GPIO_NUM_8;
 			};
 
 			class screen {
 				public:
-					constexpr static uint8_t address = 12;
-					constexpr static uint32_t frequency = 60'000'000;
+					constexpr static gpio_num_t dataCommand = GPIO_NUM_6;
+					constexpr static gpio_num_t slaveSelect = GPIO_NUM_5;
+					constexpr static gpio_num_t reset = GPIO_NUM_7;
+					constexpr static uint32_t frequency = 10'000'000;
 			};
 
 			class encoder {
 				public:
-					constexpr static gpio_num_t sw = GPIO_NUM_3;
-					constexpr static gpio_num_t slk = GPIO_NUM_2;
-					constexpr static gpio_num_t dt = GPIO_NUM_1;
+					constexpr static gpio_num_t sw = GPIO_NUM_0;
+					constexpr static gpio_num_t slk = GPIO_NUM_1;
+					constexpr static gpio_num_t dt = GPIO_NUM_2;
 			};
 
-			constexpr static gpio_num_t signal = GPIO_NUM_0;
+			constexpr static gpio_num_t signal = GPIO_NUM_3;
 	};
 }
--- main/main.cpp
+++ main/main.cpp
@@ -8,14 +8,40 @@
 #include "driver/ledc.h"
 #include "motor.h"
 
+#include <YOBA/main.h>
+#include <YOBA/UI.h>
+#include <YOBA/hardware/displays/SH1106Display.h>
+#include <YOBA/resources/fonts/PIXY10Font.h>
+#include <format>
+
+using namespace YOBA;
 using namespace pizda;
 
+// Display
+SH1106Display display {
+	constants::spi::mosi,
+	static_cast<uint8_t>(GPIO_NUM_NC),
+	constants::spi::clock,
+	constants::screen::slaveSelect,
+	constants::screen::dataCommand,
+	constants::screen::reset,
+	constants::screen::frequency
+};
+
+MonochromeRenderer renderer {};
+
+MonochromeColor colorB {false };
+MonochromeColor colorF {true };
+PIXY10Font font {};
+
+// Encoder
 Encoder encoder {
 	constants::encoder::dt,
 	constants::encoder::slk,
 	constants::encoder::sw
 };
 
+// Motor
 Motor motor {
 	constants::signal,
 	LEDC_CHANNEL_0,
@@ -23,6 +49,10 @@ Motor motor {
 	2000,
 	50
 };
+
+// Mode
+constexpr static uint16_t minPulseWidth = 10;
+constexpr static uint16_t maxPulseWidth = 1500 - minPulseWidth;
 
 uint8_t percent = 0;
 bool percentMode = true;
@@ -39,7 +69,175 @@ void updatePercent() {
 	ESP_LOGI("Main", "setPercent() percent: %f", (float) percent);
 }
 
+void encoderTick() {
+	if (!encoder.wasInterrupted())
+		return;
+
+	encoder.acknowledgeInterrupt();
+
+	if (encoder.isPressed()) {
+		ESP_LOGI("Main", "Mode: %s", percentMode ? "percent" : "pulse width");
+		percentMode = !percentMode;
+
+		// ESP_LOGI("Main", "Calibrating");
+		//
+		// motor.setPulseWidth(motor.getMaxPulseWidth());
+		// vTaskDelay(pdMS_TO_TICKS(3'000));
+		//
+		// motor.setPulseWidth(motor.getMinPulseWidth());
+	}
+	else {
+		const auto absRotation = std::abs(encoder.getRotation());
+
+		if (absRotation >= 4) {
+			ESP_LOGI("Main", "Rotation: %d", encoder.getRotation());
+
+			const auto magnitudeBig = absRotation >= 5;
+
+			if (percentMode) {
+				const auto magnitude = magnitudeBig ? 5 : 1;
+
+				percent = static_cast<uint8_t>(std::clamp<int16_t>(
+					static_cast<int16_t>(percent + (encoder.getRotation() > 0 ? magnitude : -magnitude)),
+					0,
+					100
+				));
+
+				updatePercent();
+			}
+			else {
+				const auto magnitude = magnitudeBig ? 50 : 10;
+
+				motor.setMinPulseWidth(static_cast<uint16_t>(std::clamp<int32_t>(
+					static_cast<int32_t>(motor.getMinPulseWidth()) + (encoder.getRotation() > 0 ? magnitude : -magnitude),
+					minPulseWidth,
+					maxPulseWidth
+				)));
+
+				updatePulseWidthRange();
+			}
+
+			encoder.setRotation(0);
+		}
+	}
+}
+
+void displayTick() {
+	const auto& resolution = display.getSize();
+
+	uint16_t unitsMin;
+	uint16_t unitsMax;
+	uint16_t unitsStep0;
+	uint16_t unitsStep1;
+	uint16_t unitsStep2;
+	uint16_t unitsValue;
+	float radPerUnit;
+	std::wstring modeText;
+
+	if (percentMode) {
+		unitsMin = 0;
+		unitsMax = 100;
+		unitsStep0 = 1;
+		unitsStep1 = 5;
+		unitsStep2 = 10;
+		unitsValue = static_cast<uint16_t>(percent);
+		radPerUnit = toRadians(1.5f);
+		modeText = L"%";
+	}
+	else {
+		unitsMin = minPulseWidth;
+		unitsMax = maxPulseWidth;
+		unitsStep0 = 10;
+		unitsStep1 = 50;
+		unitsStep2 = 100;
+		unitsValue = motor.getMinPulseWidth();
+		radPerUnit = toRadians(0.2f);
+		modeText = L"us";
+	}
+
+	const float valueRad = (static_cast<float>(unitsValue) - static_cast<float>(unitsMin)) * radPerUnit;
+
+	renderer.clear(&colorB);
+
+	// Mode text
+	renderer.renderString(
+		Point(
+			resolution.getWidth() / 2 - font.getWidth(modeText) / 2,
+			resolution.getHeight() - font.getHeight() + 1
+		),
+		&font,
+		&colorF,
+		modeText
+	);
+
+	// Triangle
+	const uint8_t triangleWidth = 6;
+	const uint8_t triangleHeight = 4;
+
+	renderer.renderFilledTriangle(
+		Point(resolution.getWidth() / 2 - triangleWidth / 2, 0),
+		Point(resolution.getWidth() / 2 + triangleWidth / 2, 0),
+		Point(resolution.getWidth() / 2 , triangleHeight),
+		&colorF
+	);
+
+	// Rose
+	const auto roseMarginTop = 3;
+	const auto roseRadius = resolution.getHeight() * 2 + 32;
+
+	const auto pivot = Point(
+		resolution.getWidth() / 2,
+		triangleHeight + roseMarginTop + roseRadius
+	);
+
+	// Circle
+//	renderer.renderCircle(
+//		pivot,
+//		roseRadius,
+//		&colorF
+//	);
+
+	// Lines
+	for (uint16_t arrowValue = unitsMin; arrowValue <= unitsMax; arrowValue += unitsStep0) {
+		const auto isBig = arrowValue % unitsStep2 == 0;
+		const uint8_t arrowLength = isBig ? 5 : (arrowValue % unitsStep1 == 0 ? 3 : 1);
+
+		const auto arrowVecNorm = Vector2F(0, -1).rotate(static_cast<float>(arrowValue) * radPerUnit - valueRad);
+		const auto arrowVecTo = arrowVecNorm * static_cast<float>(roseRadius);
+		const auto arrowVecFrom = arrowVecTo - arrowVecNorm * static_cast<float>(arrowLength);
+
+		renderer.renderLine(
+			pivot + (Point) arrowVecFrom,
+			pivot + (Point) arrowVecTo,
+			&colorF
+		);
+
+		if (isBig) {
+			const auto text = std::to_wstring(arrowValue);
+			const auto textSizeVec = Vector2F(font.getWidth(text), font.getHeight());
+			const auto textMargin = 2;
+			const auto textVec = arrowVecFrom - arrowVecNorm * (textSizeVec.getLength() / 2 + textMargin);
+
+			renderer.renderString(
+				Point(
+					pivot.getX() + (int32_t) (textVec.getX() - textSizeVec.getX() / 2),
+					pivot.getY() + (int32_t) (textVec.getY() - textSizeVec.getY() / 2)
+				),
+				&font,
+				&colorF,
+				text
+			);
+		}
+	}
+
+	renderer.flush();
+}
+
 extern "C" void app_main(void) {
+	// Display
+	display.setup();
+	renderer.setTarget(&display);
+
 	// Motor
 	motor.setup();
 
@@ -51,45 +249,8 @@ extern "C" void app_main(void) {
 
 	// ReSharper disable once CppDFAEndlessLoop
 	while (true) {
-		if (encoder.wasInterrupted()) {
-			encoder.acknowledgeInterrupt();
-
-			if (encoder.isPressed()) {
-				ESP_LOGI("Main", "Mode: %s", percentMode ? "percent" : "pulse width");
-				percentMode = !percentMode;
-
-				// ESP_LOGI("Main", "Calibrating");
-				//
-				// motor.setPulseWidth(motor.getMaxPulseWidth());
-				// vTaskDelay(pdMS_TO_TICKS(3'000));
-				//
-				// motor.setPulseWidth(motor.getMinPulseWidth());
-			}
-			else {
-				if (std::abs(encoder.getRotation()) > 3) {
-					if (percentMode) {
-						percent = static_cast<uint8_t>(std::clamp<int16_t>(
-							static_cast<int16_t>(percent + (encoder.getRotation() > 0 ? 5 : -5)),
-							0,
-							100
-						));
-
-						updatePercent();
-					}
-					else {
-						motor.setMinPulseWidth(static_cast<uint16_t>(std::clamp<int32_t>(
-							static_cast<int32_t>(motor.getMinPulseWidth()) + (encoder.getRotation() > 0 ? 50 : -50),
-							100,
-							1500 - 100
-						)));
-
-						updatePulseWidthRange();
-					}
-
-					encoder.setRotation(0);
-				}
-			}
-		}
+		encoderTick();
+		displayTick();
 
 		vTaskDelay(pdMS_TO_TICKS(1'000 / 30));
 	}
